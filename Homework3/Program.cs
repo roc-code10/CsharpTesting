aed13368c9091be73f80a030beffafbf9b35b367
rciceu <ciceu.raluca10@gmail.com>
2023-03-22 21:59:10 +0200

    Added Person and Soldier classes

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Homework3;
+
+Console.WriteLine("Homework3 - Play with objects");
+
+var soldier1 = new Soldier("Soldier1", 30);
+var soldier2 = new Soldier("Soldier2", 31);
+soldier1.Shooting(soldier1);
+soldier1.Shooting(soldier2);
---
+++ Homework3/Soldier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public class Soldier : Person
+    {
+        public Soldier(string name, int age) : base(name, age)
+        {
+        }
+
+        public void Shooting(Soldier enemy)
+        {
+            enemy.Health--;
+            Console.WriteLine($"Health of soldier {enemy.Name} is {enemy.Health}");
+        }
+    }
+}
