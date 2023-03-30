335670914cd05de7131507145af078423e6cfc28
rciceu <ciceu.raluca10@gmail.com>
2023-03-30 11:42:45 +0300

    Added homework in which is calculated the 47th prime number

---
+++ Homework4/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+int num = 2;
+int primeNumber = 0;
+
+while (count < 47)
+{
+    bool isPrime = true;
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+    {
+        primeNumber = num;
+        count++;
+    }
+    num++;
+}
+
+Console.WriteLine(primeNumber);
