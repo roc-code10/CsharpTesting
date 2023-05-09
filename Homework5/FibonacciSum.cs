af0aef2a8293acaabf233c5e47b4072b9102cd9d
rciceu <ciceu.raluca10@gmail.com>
2023-05-09 13:11:23 +0300

    Added Homework 4 for Basic C# Part 2. Added test for Homework 4 for Basic C# Part 2. Renamed projects.

---
+++ Homework5/FibonacciSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace BasicCSharpPart2Homework4
+{
+    public class FibonacciSum
+    {
+        public static int SumEvenValuedTermsUpToLimit(int limit)
+        {
+            //variables are initialized to the first two terms of the sequence
+            int a = 1;
+            int b = 2;
+
+            //variable is initialized to 0 to store the sum of the even-valued terms
+            int sum = 0;
+
+            while (a <= limit)
+            {
+                //checks if 'a' is even
+                if (a % 2 == 0)
+                {
+                    //if 'a' is even, adds 'a' to sum
+                    sum += a;
+                }
+
+                //calculates the next term in the sequence by adding 'a' and 'b' 
+                int c = a + b;
+                //updates the values of 'a' and 'b'
+                a = b;
+                b = c;
+            }
+            return sum;
+        }
+    }
+}
---
+++ TestProject1/BasicCSharpPart2Homework4/FibonacciSumTests.cs
@@ -0,0 +1,21 @@
+﻿using BasicCSharpPart2Homework4;
+using Xunit;
+
+namespace Tests.BasicC_Part2Homework4
+{
+    public class FibonacciSumTests
+    {
+        [Fact]
+        public void SumEvenValuedTermsUpToLimit_ReturnsCorrectSum()
+        {
+            //arrange
+            int expectedSum = 798;
+
+            //act
+            int actualSum = FibonacciSum.SumEvenValuedTermsUpToLimit(1000);
+
+            //assert
+            Assert.Equal(expectedSum, actualSum);
+        }
+    }
+}
